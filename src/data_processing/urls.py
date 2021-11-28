d99555a5b4e656ff6d8a170784dbfbb6f2d7b273
maxweaker <455436082@qq.com>
2021-11-29 01:28:01 +0800

    index build and import to elastic

--- src/data_processing/urls.py
+++ src/data_processing/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('read_data', views.read_data, name='read_data'),
     path('clear_data', views.clear_data, name='clear_data'),
     path('clean_data', views.clean_data, name='clean_data'),
+    path('build_index', views.build_index, name='build_index'),
+    path('test', views.test, name='test'),
+    path('import_doc', views.import_doc, name='import_doc'),
+
 ]
