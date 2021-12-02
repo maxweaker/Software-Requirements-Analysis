b5110f3e0c4f5114a2609c4ea93fb0da021a2fb4
katytught <yu.000423@139.com>
2021-12-02 21:24:16 +0800

    time

--- src/pattern/models.py
+++ src/pattern/models.py
@@ -3,28 +3,29 @@ from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 import re
 from django.core import validators
+
+
 # Create your models here.
 
 class Document(models.Model):
-    DocID = models.CharField(max_length=64,primary_key=True) #数据源自带字段
+    DocID = models.CharField(max_length=64, primary_key=True)  # 数据源自带字段
 
-    firstKeyword = models.ForeignKey('Keyword',on_delete=models.CASCADE,null=False)
-    firstAuthor = models.ForeignKey('Author',on_delete=models.CASCADE,null=False)
-    firstField = models.ForeignKey('Field',on_delete=models.CASCADE,null=True,default=None)
-    firstRefer = models.ForeignKey('Refer',on_delete=models.CASCADE,null=True,default=None)
+    firstKeyword = models.ForeignKey('Keyword', on_delete=models.CASCADE, null=False)
+    firstAuthor = models.ForeignKey('Author', on_delete=models.CASCADE, null=False)
+    firstField = models.ForeignKey('Field', on_delete=models.CASCADE, null=True, default=None)
+    firstRefer = models.ForeignKey('Refer', on_delete=models.CASCADE, null=True, default=None)
 
-    DOI = models.CharField(max_length=64,null=False,unique=True)
-    type = models.CharField(max_length=20,null=True)
+    DOI = models.CharField(max_length=64, null=False, unique=True)
+    type = models.CharField(max_length=20, null=True)
 
-    title = models.TextField(null=False,blank=False)
-    abstract = models.TextField(blank=True,null=True)
+    title = models.TextField(null=False, blank=False)
+    abstract = models.TextField(blank=True, null=True)
 
     mainBody = models.FileField(null=True)
 
-    pubYear = models.IntegerField(null=True,default=1900)
+    pubYear = models.IntegerField(null=True, default=1900)
     citation = models.IntegerField(default=0)
     vist = models.IntegerField(default=0)
-
 
     def getKeywords(self):
         keys = []
@@ -72,54 +73,65 @@ class Document(models.Model):
                 break
         return refers
 
+
 class User(models.Model):
-    nickname = models.CharField(max_length=64,null=False)
-    password = models.CharField(max_length=64,null=False)
+    nickname = models.CharField(max_length=64, null=False)
+    password = models.CharField(max_length=64, null=False)
     email = models.EmailField(null=False)
+
     def __str__(self):
         return self.nickname
 
+
 class Mechanism(User):
-    mecName = models.CharField(max_length=128,null=False)
+    mecName = models.CharField(max_length=128, null=False)
     briefIntro = models.TextField(null=True)
-    ranking  = models.IntegerField(null=True)
+    ranking = models.IntegerField(null=True)
+
 
 class Expert(User):
-    EID= models.CharField(max_length=32,primary_key=True)
+    EID = models.CharField(max_length=32, primary_key=True)
 
-    mec = models.ForeignKey('Mechanism',on_delete=models.CASCADE,null=True)
-    
+    mec = models.ForeignKey('Mechanism', on_delete=models.CASCADE, null=True)
+
     domain = models.CharField(max_length=64)
     realName = models.CharField(max_length=128)
-    pubNum  = models.IntegerField(null=True)
-    isCertified = models.BooleanField(default=False,null=False)
+    pubNum = models.IntegerField(null=True)
+    isCertified = models.BooleanField(default=False, null=False)
+
 
 class Administrators(User):
     realName = models.CharField(max_length=64)
 
+
 class Keyword(models.Model):
-    nextKey = models.ForeignKey('Keyword',on_delete=models.CASCADE,null=True,default=None)
-    key = models.CharField(max_length=64,null=False)
+    nextKey = models.ForeignKey('Keyword', on_delete=models.CASCADE, null=True, default=None)
+    key = models.CharField(max_length=64, null=False)
+
 
 class Field(models.Model):
-    nextField = models.ForeignKey('Field',on_delete=models.CASCADE,null=True,default=None)
-    fieldName = models.CharField(max_length=64,null=False)
+    nextField = models.ForeignKey('Field', on_delete=models.CASCADE, null=True, default=None)
+    fieldName = models.CharField(max_length=64, null=False)
+
 
 class Author(models.Model):
-    nextAuthor = models.ForeignKey('Author',on_delete=models.CASCADE,null=True,default=None)
-    name = models.CharField(max_length=64,null=False,default='unknow')
+    nextAuthor = models.ForeignKey('Author', on_delete=models.CASCADE, null=True, default=None)
+    name = models.CharField(max_length=64, null=False, default='unknow')
+
 
 class Refer(models.Model):
-    nextRefer = models.ForeignKey('Refer',on_delete=models.CASCADE,null=True,default=None)
-    DocID = models.CharField(max_length=64,null=True)
+    nextRefer = models.ForeignKey('Refer', on_delete=models.CASCADE, null=True, default=None)
+    DocID = models.CharField(max_length=64, null=True)
+
 
 class Comment(models.Model):
-    nextComment = models.ForeignKey('Comment',on_delete=models.CASCADE,null=True)
+    nextComment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True)
     userName = models.CharField(max_length=64)
 
-class Attestation(models.Model):
-    applicant = models.ForeignKey('User',on_delete=models.CASCADE)
 
-    mecName = models.CharField(max_length=128,null=False)
-    operatorID = models.CharField(max_length=32,null=False)
+class Attestation(models.Model):
+    applicant = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    mecName = models.CharField(max_length=128, null=False)
+    operatorID = models.CharField(max_length=32, null=False)
     certificateFile = models.FileField(null=False)
--- src/pattern/views.py
+++ src/pattern/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import simplejson
+from .models import User
+import json
+from pattern.multiplexing_operation import *
+
+from elasticsearch import Elasticsearch, helpers
+from django.shortcuts import render
+
+
+def reladata(request):
+    if request.method == 'GET':
+        req = simplejson.loads(request)
+        nickname = req.session['nickname']
+        users = User.objects.filter(nickname=nickname)
+        if not users:
+            return HttpResponse(json.dumps({
+                'success': 0,
+            }))
+        user = users[0]
+        data = {
+            'userName': user.nickname,
+            'userType': user.usertype,
+            'isQualified': user.isqualified,
+            'selfIntroduce': user.selfintroduce,
+        }
+        return HttpResponse(json.dumps(data), status=200)
+
+
+def linechart(request):
+    if request.method == 'GET':
+        req = simplejson.loads(request)
+        nickname = req.session['nickname']
+        users = User.objects.filter(nickname=nickname)
+        if not users:
+            return HttpResponse(json.dumps({
+                'success': 0,
+            }))
+        user = users[0]
+        data = {
+            'date':user.data,
+            'watchNum':user.watchnum,
+            'type':user.type,
+        }
+        return HttpResponse(json.dumps(data))
+
+
 
 # Create your views here.
